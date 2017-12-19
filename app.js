33729e421659671d1d9b91a1aed2faf7ad65ec14
ppim <31943128+ppim@users.noreply.github.com>
2017-12-19 17:39:35 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const graphContainer = document.getElementById('graphContainer')
+const graph = document.createElement('img')
+let curImg = './img/10-14.svg'
+let img = curImg
+
+graph.className = 'graph-img animated fadeIn'
+
+function createGraph() {
+    graph.setAttribute('src', curImg)
+    graphContainer.appendChild(graph)
+}
+
+createGraph()
+
+window.onscroll = function() {
+    const vh = window.innerHeight
+    const startPos = vh
+    const firstChangePos = startPos + vh
+    const secondChangePos = firstChangePos + vh
+    const scrollPos = window.scrollY
+    
+    img = curImg
+
+    if(scrollPos <= firstChangePos) {
+        curImg = './img/10-14.svg'
+    } else if(scrollPos > firstChangePos && scrollPos < secondChangePos) {
+        curImg = './img/15-17.svg'
+    } else if(scrollPos >= secondChangePos) {
+        curImg = './img/18-19.svg'
+    } 
+
+    if(img != curImg) {
+        graphContainer.innerHTML = ''
+        createGraph()
+    }
+}
